852c73989092fd3f02b55717162b09a6eeb69969
Aktonen <aku.tolonen1999@hotmail.fi>
2023-04-19 22:30:02 +0300

    Started working on main gameboard

---
+++ components/Game.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import {Text, View, TextInput, Pressable } from "react-native";
+import styles from "../styles/styles";
+
+export default function Game() {
+    return (
+        <View>
+            <Image source={require("../images/Tell_a_joke.png")}></Image>
+        </View>
+    )
+}
