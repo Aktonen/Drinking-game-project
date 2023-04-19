dd57752db420d7f8725f4740345bf94e92b6ab54
Aktonen <aku.tolonen1999@hotmail.fi>
2023-04-19 22:29:29 +0300

    Styling

--- styles/styles.js
+++ styles/styles.js
@@ -2,6 +2,35 @@ import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
 
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+
+    title: {
+        fontSize: 30,
+        padding: 10
+    },
+
+    startButton: {
+        borderWidth: 2,
+        borderRadius: 10,
+        margin: 20,
+        width: 160,
+        height: 50,
+        justifyContent: "center",
+    },
+
+    buttonText: {
+        color: "white",
+        padding: 5,
+        textAlign: "center",
+        fontSize: 20
+
+    },
+
     playerInput: {
         borderWidth: 1,
         width: 200,
