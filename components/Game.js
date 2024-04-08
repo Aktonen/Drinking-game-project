f823859f11d4fd7ac9b3605874f8ca5265dac1d5
Aktonen <aku.tolonen1999@hotmail.fi>
2024-04-08 07:06:20 +0300

    We display the image from an array now.

--- components/Game.js
+++ components/Game.js
@@ -5,9 +5,21 @@ import Icon from 'react-native-vector-icons/AntDesign';
 
 
 export default function Game() {
+
+
+  // These have to be changed if the app is published
+  const IMAGES = [
+    "http://192.168.0.104:19000/images/Category_dogs.jpg",
+    "http://192.168.0.104:19000/images/Lie_on_your_back.jpg",
+    "http://192.168.0.104:19000/images/Old_friends.jpg",
+    "http://192.168.0.104:19000/images/Piercing.jpg",
+    "http://192.168.0.104:19000/images/Tell_a_joke.png"
+  ]
+
+
   return (
     <View style={styles.gameScreen}>
-      <Image source={require("../images/Old_friends.jpg")} style={styles.gameImage}></Image>
+      <Image source={{uri:IMAGES[0]}} style={styles.gameImage}></Image>
       <Pressable style={styles.gameArrowRight}>
         <Icon name="arrowright" size={100} color="black" />
       </Pressable>
